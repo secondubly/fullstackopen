74fd8d609b88cba3a1248cc8f5a16fffa42a1a2e
secondubly <secondubly@gmail.com>
2025-11-13 14:56:14 -0800

    complete 1.12

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,39 +1,32 @@
-import { useState } from "react"
-import Display from "./components/Display"
-import Button from "./components/Button"
-import Statistics from "./components/Statistics"
+import { useState } from 'react'
 
 const App = () => {
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+   
+  const [selected, setSelected] = useState(0)
 
-  
-  const updateGood = () => {
-    const goodNewValue = good + 1
-    setGood(goodNewValue)
+  const pickRandomQuote = () => {
+    const quoteLength = anecdotes.length
+    const quoteNumber = Math.floor(Math.random() * quoteLength)
+    console.log(quoteNumber)
+    setSelected(quoteNumber)
   }
 
-  const updateNeutral = () => {
-    const neutralNewValue = neutral + 1
-    setNeutral(neutralNewValue)
-  }
-
-  const updateBad = () => {
-    const badNewValue = bad + 1
-    setBad(badNewValue)
-  }
-  
   return (
     <div>
-      <Display text="give feedback" />
-      <br />
-      <Button onClick={updateGood} text="good" />&nbsp;
-      <Button onClick={updateNeutral} text="neutral" />&nbsp;
-      <Button onClick={updateBad} text="bad" />
-      <br />
-      <Display text="statistics" />
-      <Statistics good={good} neutral={neutral} bad={bad} />
+      {anecdotes[selected]}<br />
+    <button onClick={pickRandomQuote}>
+        next anecdote
+    </button>
     </div>
   )
 }
